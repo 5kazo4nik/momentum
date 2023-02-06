6932d5101e3f824012a90a941342e4d9aaa31f0a
Oleg <alhimov.oleg2012@yandex.ru>
2023-02-06 23:08:08 +0300

    feat: 'first webpack adjust'

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import { hello } from './js/console';
+
+window.onload = function () {
+  console.log('Hello wor!s');
+  // console.log('Hello wor!s');
+  hello();
+};
---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const webpack = require('webpack');
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = (env, options) => {
+  const isProduction = options.mode === 'production';
+  const config = {
+    mode: isProduction ? 'production' : 'development',
+    devtool: isProduction ? false : 'source-map',
+    watch: !isProduction,
+    watchOptions: {
+      poll: true,
+      ignored: /node_modules/,
+    },
+    entry: ['./src/index.js', './src/css/owfont-regular.css', './src/css/style.css'],
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'script.js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
+        {
+          test: /\.css$/,
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        },
+        {
+          test: /\.(ico|png|svg|jpe?g|gif|mp3)$/,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.html$/,
+          loader: 'html-loader',
+        },
+      ],
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: 'index.html',
+      }),
+      new MiniCssExtractPlugin({
+        filename: 'style.css',
+      }),
+    ],
+  };
+  return config;
+};
